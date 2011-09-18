b2b6c7b37d16f15b6ad195d433c744a31bede822
Jason Wiener <hiya@jasonwiener.com>
2011-09-17 22:02:18 -0700

    This is the first commit since forking node-bisection from 3rd-Eden. It includes a full refactor of the source as well as functional fixes, YUI-minified source, and the addition of the insort functions.
    Started with fixing a basic functional problem in the original source
    where one of the functions was no returning positions as expected or
    as the algorithm was originally designed.  Using Python 2.5.2, I ran
    the tests 3rd-Eden built and used those as the baseline for testing
    and revising the source.
    
    Once completed, I added the insort functionality that is also available
    in the Python module.
    
    I've also included a YUI-minified version of the source.  Either version
    can be utilized in node.js or non-node.js projects.
    
    Finally, I ran benchmarks against the node-bisection refactor that I'm
    committing against identical tests on Python.  node-bistection ran >2x
    faster of node.js for insort functions and >4x faster on bisect operations.
    Being this, my first port and experience with node.js and v8, I definitely
    must admit being heavily impressed. I use bisect a TON in projects and
    seeing the performance differences has made me want to explore node.js
    more as well as v8.
    
    Finally the test data. I ran identical timed loop operations (looping
    100000x) over a funciton where a master array (living outside the loop)
    is duplicated using the fastest method possible to my knowledge. On
    node.js I used .splice(0) and in Python I used [:].  Then I used the
    copied list against each of the functions 1 at a time. First testing
    insort_left 100000x, and recording the time, then insort_right, recording,
    etc.
    
    The results:
    node.js - 100000x
    insort_left trt: 41ms
    insort_right trt: 40ms
    bisect_left trt: 24ms
    bisect_right trt: 20ms
    
    python 2.5.2 - 100000x
    insort_left test took 110.000ms
    insort_right test took 110.000ms
    bisect_left test took 100.000ms
    bisect_right test took 100.000ms
    
    Hope this helps some folks as I'll be using the code a ton in my JS
    projects going forward without a doubt.

---
+++ README.md
@@ -0,0 +1,117 @@
+# Node-Bisection
+
+Node-Bisection is a JavaScript port of the Python 8.5. bisect - Array bisection algorithm.
+&lt;http://docs.python.org/library/bisect.html&gt;
+
+This module provides support for maintaining a list in sorted order without having to sort 
+the list after each insertion. For long lists of items with expensive comparison operations,
+this can be an improvement over the more common approach. The module is called bisect because
+it uses a basic bisection algorithm to do its work. The source code may be most useful as a 
+working example of the algorithm (the boundary conditions are already right!)."
+
+This project is not limited to use in node.js. It's excellent for any JavaScript project. Also 
+included in the repo is a YUI-minified version of the code that's pretty-wicked @ only 527 bytes!
+
+## How to use
+
+### in node.js
+
+Require `bisection`:
+
+```js
+var bisect = require('bisection');
+```
+
+### In a Mozilla Firefox add-on project.
+
+Inside the main xul file for the project, add this line after the opening <overlay> tag. This 
+follows that you've included bisection.js in the content folder of the add-ons folder structure.
+
+```html
+<script type="application/x-javascript" src="chrome://PROJECT_NAME/content/bisection.js"/>
+```
+
+### In a web page
+
+Add this line to inside the <head> tag of your web page. It assumes that the bisection.js is 
+in the same folder as this page.
+
+```html
+<script type="text/javascript" src="bisection.js" ></script>
+```
+
+In node.js projects, you're ready to start using `bisection`.
+
+In other project instantiate the object to a variable by:
+
+```js
+var bisect = new bisection();
+```
+Then, simply call the `bisection` function you want to use.
+
+To find the index where this item should go in the list, do:
+```js
+bisect.bisect(an_array, an_item);
+```
+
+To insert an item in place, do:
+```js
+bisect.insort(an_array, an_item);
+```
+
+## Short recipes
+
+### node.js projects. - Bisect an array.
+
+```js
+var bisect = require('bisection');
+var an_array = [0,1,2,3,5,6,7,8,9];
+console.log('bisect.bisect: ' + bisect.bisect(an_array, 5));
+console.log('bisect.bisect_left: ' + bisect.bisect_left(an_array, 5));
+```
+
+outputs:
+```
+bisect.bisect: 5
+bisect.bisect_left: 4
+```
+
+### Non-node.js projects. - Bisect an array.
+
+```js
+var bisect = new bisection();
+var an_array = [0,1,2,3,5,6,7,8,9];
+console.log('bisect.bisect: ' + bisect.bisect(an_array, 5));
+console.log('bisect.bisect_left: ' + bisect.bisect_left(an_array, 5));
+```
+
+outputs:
+```
+bisect.bisect: 5
+bisect.bisect_left: 4
+```
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2011 Jason Wiener, JasonWiener, LLC &lt;hiya@jasonwiener.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- tests/bisection.test.js
+++ tests/bisection.test.js
@@ -1,36 +1,191 @@
-var should = require('should')
-  , bisection = require('../lib/bisection');
+/**
+ * Test results from Python 2.5.2 bisect
+ * 
+ * Note: The arrow next to each call has been added to show the index 
+ * position that the method produced.
+ * 
+ * 	bisect_right
+ * 	>>> bisect.bisect_right(small, 0) -> 1
+ * 	>>> bisect.bisect_right(small, 7) -> 7
+ * 	>>> bisect.bisect_right(small, 15) -> 9
+ * 	>>> bisect.bisect_right(small, 4, 5) -> 5
+ * 	>>> bisect.bisect_right(small, 12, 0, 5) -> 5
+ * 
+ * 	>>> bisect.bisect_right(large, 0) -> 0
+ * 	>>> bisect.bisect_right(large, 242) -> 4
+ * 	>>> bisect.bisect_right(large, 350) -> 8
+ * 	>>> bisect.bisect_right(large, 232424) -> 14
+ * 
+ * 	bisect_left
+ * 	>>> bisect.bisect_left(small, 0) -> 0
+ * 	>>> bisect.bisect_left(small, 7) -> 6
+ * 	>>> bisect.bisect_left(small, 15) -> 9
+ * 	>>> bisect.bisect_left(small, 4, 5) -> 5
+ * 	>>> bisect.bisect_left(small, 12, 0, 5) -> 5
+ * 	
+ * 	>>> bisect.bisect_left(large, 0) -> 0
+ * 	>>> bisect.bisect_left(large, 242) -> 4
+ * 	>>> bisect.bisect_left(large, 350) -> 8
+ * 	>>> bisect.bisect_left(large, 232424) -> 14
+ * 	
+ * 	insort_right
+ * 	>>> bisect.insort_right(small,0) -> 1
+ * 	[0, 0, 1, 2, 3, 5, 6, 7, 8, 9]
+ *      ^
+ * 	>>> bisect.insort_right(small,7) -> 7
+ * 	[0, 1, 2, 3, 5, 6, 7, 7, 8, 9]
+ *                        ^
+ * 	>>> bisect.insort_right(small,15) -> 9
+ * 	[0, 1, 2, 3, 5, 6, 7, 8, 9, 15]
+ *                              ^^
+ * 	>>> bisect.insort_right(small, 4, 5) -> 5
+ * 	[0, 1, 2, 3, 5, 4, 6, 7, 8, 9]
+ *                  ^
+ * 	>>> bisect.insort_right(small, 12, 0, 5) -> 5
+ * 	[0, 1, 2, 3, 5, 12, 6, 7, 8, 9]
+ * 	                ^^
+ * 	>>> bisect.insort_right(large, 0) -> 0
+ * 	[0, 122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]
+ *   ^
+ * 	>>> bisect.insort_right(large, 242) -> 4
+ * 	[122, 124, 134, 176, 242, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]
+ *                       ^^^
+ * 	>>> bisect.insort_right(large, 350) -> 8
+ * 	[122, 124, 134, 176, 320, 341, 345, 346, 350, 500, 587, 666, 768, 1230, 23231]
+ *                                           ^^^
+ * 	>>> bisect.insort_right(large, 232424) -> 14
+ * 	[122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231, 232424]
+ * 	                                                                          ^^^^^^
+ * 	>>> bisect.insort_left(small, 0) -> 0
+ * 	[0, 0, 1, 2, 3, 5, 6, 7, 8, 9]
+ *   ^
+ * 	>>> bisect.insort_left(small, 7) ->  6
+ * 	[0, 1, 2, 3, 5, 6, 7, 7, 8, 9]
+ *                     ^
+ * 	>>> bisect.insort_left(small, 15) -> 9
+ * 	[0, 1, 2, 3, 5, 6, 7, 8, 9, 15]
+ *                              ^^
+ * 	>>> bisect.insort_left(small, 4, 5) -> 5
+ * 	[0, 1, 2, 3, 5, 4, 6, 7, 8, 9]
+ *                  ^
+ * 	>>> bisect.insort_left(small, 12, 0, 5) -> 5
+ * 	[0, 1, 2, 3, 5, 12, 6, 7, 8, 9]
+ *                  ^^
+ * 	>>> bisect.insort_left(large, 0) -> 0
+ * 	[0, 122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]
+ *   ^
+ * 	>>> bisect.insort_left(large, 242) -> 4
+ * 	[122, 124, 134, 176, 242, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]
+ *                       ^^^
+ * 	>>> bisect.insort_left(large, 350) -> 8
+ * 	[122, 124, 134, 176, 320, 341, 345, 346, 350, 500, 587, 666, 768, 1230, 23231]
+ *                                           ^^^
+ * 	>>> bisect.insort_left(large, 232424) -> 14
+ * 	[122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231, 232424]
+ * 	                                                                          ^^^^^^
+ */
 
-var small = [0,1,2,3,5,6,7,8,9]
-  , large = [122,124,134,176,320,341,345,346,500,587,666,768,1230,23231];
+var should = require('should'), bisection = require('../lib/bisection');
+
+var small = [0,1,2,3,5,6,7,8,9],
+	large = [122,124,134,176,320,341,345,346,500,587,666,768,1230,23231];
 
 module.exports = {
-  'Library version': function(){
+
+  'library version': function(){
      bisection.version.should.match(/^\d+\.\d+\.\d+$/);
-  }
-, 'Bisection and bisection right': function(){
-     bisection.should.equal(bisection.right);
-  }
-, 'Bisection right': function(){
-    bisection(small, 4).should.equal(4);
-    bisection(small, 15).should.equal(9);
-    bisection(small, 4, 5).should.equal(5);
-    bisection(small, 12, 0, 5).should.equal(5);
+  },
+  
+  'bisect and bisect_right': function(){
+     bisection.bisect.should.equal(bisection.bisect_right);
+  },
+  
+  'bisect right': function(){
+    bisection.bisect(small, 0).should.equal(1);
+    bisection.bisect(small, 7).should.equal(7);
+    bisection.bisect(small, 15).should.equal(9);
+    bisection.bisect(small, 4, 5).should.equal(5);
+    bisection.bisect(small, 12, 0, 5).should.equal(5);
     
-    bisection(large, 350).should.equal(8);
-    bisection(large, 242).should.equal(4);
-    bisection(large, 0).should.equal(0);
-    bisection(large, 232424).should.equal(14);
-  }
-, 'Bisection left': function(){
-    bisection.left(small, 4).should.equal(9);
-    bisection.left(small, 15).should.equal(0);
-    bisection.left(small, 4, 5).should.equal(9);
-    bisection.left(small, 12, 0, 5).should.equal(0);
+    bisection.bisect(large, 0).should.equal(0);
+    bisection.bisect(large, 242).should.equal(4);
+    bisection.bisect(large, 350).should.equal(8);
+    bisection.bisect(large, 232424).should.equal(14);
+  },
+
+  'bisect left': function(){
+    bisection.bisect_left(small, 0).should.equal(0);
+    bisection.bisect_left(small, 7).should.equal(6);
+    bisection.bisect_left(small, 15).should.equal(9);
+    bisection.bisect_left(small, 4, 5).should.equal(5);
+    bisection.bisect_left(small, 12, 0, 5).should.equal(5);
     
-    bisection.left(large, 350).should.equal(0);
-    bisection.left(large, 242).should.equal(14);
-    bisection.left(large, 0).should.equal(14);
-    bisection.left(large, 232424).should.equal(0);
+    bisection.bisect_left(large, 0).should.equal(0);
+    bisection.bisect_left(large, 242).should.equal(4);
+    bisection.bisect_left(large, 350).should.equal(8);
+    bisection.bisect_left(large, 232424).should.equal(14);
+  },
+  
+  'insort right': function(){
+	
+	_small = small.slice(0);
+	bisection.insort(_small, 0);
+    _small.should.eql([0, 0, 1, 2, 3, 5, 6, 7, 8, 9]);
+	_small = small.slice(0);
+	bisection.insort(_small, 7);
+	_small.should.eql([0, 1, 2, 3, 5, 6, 7, 7, 8, 9]);
+	_small = small.slice(0);
+	bisection.insort(_small, 15);
+	_small.should.eql([0, 1, 2, 3, 5, 6, 7, 8, 9, 15]);
+	_small = small.slice(0);
+    bisection.insort(_small, 4, 5);
+    _small.should.eql([0, 1, 2, 3, 5, 4, 6, 7, 8, 9]);
+	_small = small.slice(0);
+    bisection.insort(_small, 12, 0, 5);
+    _small.should.eql([0, 1, 2, 3, 5, 12, 6, 7, 8, 9]);
+
+	_large = large.slice(0);
+    bisection.insort(_large, 0);
+    _large.should.eql([0, 122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]);
+	_large = large.slice(0);
+	bisection.insort(_large, 242);
+	_large.should.eql([122, 124, 134, 176, 242, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]);
+	_large = large.slice(0);
+	bisection.insort(_large, 350);
+	_large.should.eql([122, 124, 134, 176, 320, 341, 345, 346, 350, 500, 587, 666, 768, 1230, 23231]);
+	_large = large.slice(0);
+	bisection.insort(_large, 232424);
+	_large.should.eql([122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231, 232424]);
+  },
+  
+  'insort left': function(){
+	_small = small.slice(0);
+	bisection.insort_left(_small, 0);
+    _small.should.eql([0, 0, 1, 2, 3, 5, 6, 7, 8, 9]);
+	_small = small.slice(0);
+	bisection.insort_left(_small, 7);
+	_small.should.eql([0, 1, 2, 3, 5, 6, 7, 7, 8, 9]);
+	_small = small.slice(0);
+	bisection.insort_left(_small, 15);
+	_small.should.eql([0, 1, 2, 3, 5, 6, 7, 8, 9, 15]);
+	_small = small.slice(0);
+    bisection.insort_left(_small, 4, 5);
+    _small.should.eql([0, 1, 2, 3, 5, 4, 6, 7, 8, 9]);
+	_small = small.slice(0);
+    bisection.insort_left(_small, 12, 0, 5);
+    _small.should.eql([0, 1, 2, 3, 5, 12, 6, 7, 8, 9]);
+
+	_large = large.slice(0);
+    bisection.insort_left(_large, 0);
+    _large.should.eql([0, 122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]);
+	_large = large.slice(0);
+	bisection.insort_left(_large, 242);
+	_large.should.eql([122, 124, 134, 176, 242, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231]);
+	_large = large.slice(0);
+	bisection.insort_left(_large, 350);
+	_large.should.eql([122, 124, 134, 176, 320, 341, 345, 346, 350, 500, 587, 666, 768, 1230, 23231]);
+	_large = large.slice(0);
+	bisection.insort_left(_large, 232424);
+	_large.should.eql([122, 124, 134, 176, 320, 341, 345, 346, 500, 587, 666, 768, 1230, 23231, 232424]);
   }
 }
